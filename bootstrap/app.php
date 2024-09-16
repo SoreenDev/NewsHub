3f642650c4adf12f5789be6df7d6ea6d659deae8
MJ-h <soreendev@gmail.com>
2024-09-16 15:58:43 +0330

    fix bug ddd structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->useAppPath("src/App");
+
+return $app;
